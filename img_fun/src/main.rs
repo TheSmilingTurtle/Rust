c96cb683dfcde4974c6ecae2cf118c5af51de236
TheSmilingTurtle <vojta.kottas@gmail.com>
2022-08-02 18:50:06 +0200

    sync

--- img_fun/src/main.rs
+++ img_fun/src/main.rs
@@ -1,39 +1,45 @@
 use image::{ RgbImage, Rgb };
 use num::Complex;
 
-const SIZE: (u32, u32) = (2000, 1000);
-const TOTAL: (usize, usize) = (4, 2);
-const OFFSET: (isize, isize) = (2, -1);
+const SIZE: (u32, u32) = (30_000, 40_000);
+const SIDELENGTHS: (f64, f64) = (3., 4.);
+const OFFSET: (f64, f64) = (-1.5, 2.);
 
-fn colour(i: usize) -> Rgb<u8> {
-    Rgb( [ (i * 7 + 20) as u8, (i * i + 20) as u8, (i * i / 15 + 50) as u8 ] )
+const fn colour(i: usize) -> Rgb<u8> {
+    Rgb( [255 - 5 * i as u8, 255 - 5 * i as u8, 255 - 5 * i as u8] )
+    //Rgb( [ (i * 7 + 20) as u8, (i * i + 20) as u8, (i * i / 15 + 50) as u8 ] )
 
     //Rgb( [ (i * 5 + 100) as u8, (i * 5) as u8, (i * 5 + 100) as u8] )
 }
 
 fn pixel_to_point(x: f64, y: f64) -> Complex<f64> {
     Complex::new(
-        (x/(SIZE.0 as f64) * TOTAL.0 as f64) - OFFSET.0 as f64,
-        (-y/(SIZE.1 as f64) * TOTAL.1 as f64) - OFFSET.1 as f64
+        (x/(SIZE.0 as f64) * SIDELENGTHS.0 as f64) + OFFSET.0,
+        (-y/(SIZE.1 as f64) * SIDELENGTHS.1 as f64) + OFFSET.1
     )
 }
 
 fn func(x: u32, y: u32) -> Rgb<u8> {
-    let mut z = Complex::new(0.5, 0.);
-    let point = Complex::new(1.0, 0.);
-    let constant = pixel_to_point(x as f64, y as f64);
+    let mut z = Complex::new( 0., 0.);
+    let _p = Complex::new(1.0, 0.);
+    let c = pixel_to_point(x as f64, y as f64);
 
-    for i in 0..50 {
-        z = constant * z * (point - z);
-        if z.norm() > 2. {
+    for i in 0..51 {
+
+        z = {
+            c * (z.exp() - z)
+        };
+
+        if z.norm_sqr() > 4. {
             return colour(i);
         }
     }
 
-    colour(50)
+    colour(51)
 }
 
 fn main() {
+    
     let start = std::time::Instant::now();
 
     let image = RgbImage::from_fn(
